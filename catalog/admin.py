e44add61f60e40abc520de9b7b3ece68815bd9eb
aminMuktar <aminmuktar41@gmail.com>
2023-11-22 16:30:17 +0300

    created author and book model + static files

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Author, Book
+
+classes = [Author, Book]
+
+for model in classes:
+    admin.site.register(model)
